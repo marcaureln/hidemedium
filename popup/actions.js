2c870dc890a83e3fd87def17d341e36d69207bba
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-07-12 17:41:40 +0000

    add show option

--- popup/actions.js
+++ popup/actions.js
@@ -1,3 +1,41 @@
 'use strict';
 
-document.getElementById('number').innerHTML = 10;
+// document.getElementById('number').innerHTML = 0;
+
+let options = {
+	hide: true,
+	blur: false,
+	disableAll: () => {
+		options.hide = false;
+		options.blur = false;
+	},
+	setHide: () => {
+		options.hide = true;
+		options.blur = false;
+	},
+	setBlur: () => {
+		options.blur = true;
+		options.hide = false;
+	},
+};
+
+document.addEventListener('click', () => {
+	let radios = document.getElementsByTagName('input');
+	for (let i = 0; i < radios.length; i++) {
+		const element = radios[i];
+		if (element.checked) {
+			switch (element.value) {
+				case 'show':
+					options.disableAll();
+					break;
+				case 'hide':
+					options.setHide();
+					break;
+				case 'blur':
+					options.setBlur();
+					break;
+			}
+			break;
+		}
+	}
+});
