d61b09d87744771d0aa0be4ff3c0286b1aa624de
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-07-12 17:42:51 +0000

    refactoring

--- content_scripts/app.js
+++ content_scripts/app.js
@@ -1,48 +1,75 @@
 'use strict';
 
 let hiden = 0;
+let starredArticles = [];
 
 function getArticles() {
-    return document.querySelectorAll('article');
+	return document.querySelectorAll('article');
 }
 
 function getStarredArticles() {
-    let articles = getArticles();
-    let starredArticles = [];
-    for (let i = 0; i < articles.length; i++) {
-        if (articles[i].style.display != 'none') {
-            starredArticles.push(articles[i]);
-        }
-    }
-    return starredArticles;
+	let articles = getArticles();
+	let starredArticles = [];
+	for (let i = 0; i < articles.length; i++) {
+		if (
+			articles[i].getElementsByClassName('svgIcon--star').length != 0 &&
+			!articles[i].style.display.includes('none')
+		) {
+			starredArticles.push(articles[i]);
+		}
+	}
+	return starredArticles;
 }
 
-function hideStaredArticles(articles) {
-    if (articles == []) {
-        return 0;
-    }
+function hideArticles(articles) {
+	if (articles == []) {
+		return 0;
+	}
 
-    let count = 0;
-    for (let i = 0; i < articles.length; i++) {
-        if (articles[i].getElementsByClassName('svgIcon--star').length != 0) {
-            if (articles[i].style.display.includes('!important')) {
-                articles[i].style.display = 'none !important';
-            } else {
-                articles[i].style.display = 'none';
-            }
-            // articles[i].style.filter = 'blur(1.5rem)';
-            count++;
-        }
-    }
-    return count;
+	let count = 0;
+	for (let i = 0; i < articles.length; i++) {
+		if (articles[i].style.display.includes('!important')) {
+			articles[i].style.display = 'none !important';
+		} else {
+			articles[i].style.display = 'none';
+		}
+		starredArticles.push(articles[i]);
+		count++;
+	}
+	console.log('+', count, ' now !');
+	return count;
+}
+
+function blurArticles(articles) {
+	if (articles == []) {
+		return 0;
+	}
+
+	let count = 0;
+	for (let i = 0; i < articles.length; i++) {
+		articles[i].style.filter = 'blur(1.5rem)';
+		starredArticles.push(articles[i]);
+		count++;
+	}
+	console.log('+', count, ' now !');
+	return count;
+}
+
+function updateCounter() {}
+
+function showArticles(articles) {
+	// Remove effects on each articles
+	for (let i = 0; i < articles.length; i++) {
+		articles[i].style.filter = '';
+		articles[i].style.display = '';
+	}
+	return [];
 }
 
 document.body.onload = function () {
-    hiden += hideStaredArticles(getStarredArticles());
-    console.log(hiden, ' hiden articles on load');
+	hiden += hideArticles(getStarredArticles());
 };
 
 document.body.onscroll = function () {
-    hiden += hideStaredArticles(getStarredArticles());
-    console.log(hiden, ' hiden now !');
+	hiden += hideArticles(getStarredArticles());
 };
