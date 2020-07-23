8af1bbc94a8f3b3334b8224502727f09963c2619
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-07-23 01:24:29 +0000

    switch icon

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
 
 let hide = true;
 
-browser.pageAction.onClicked.addListener(function () {
+browser.pageAction.onClicked.addListener(function (tab) {
 	console.log('clicked');
 	if (hide) {
 		console.log('hide');
 		browser.tabs.executeScript(null, { file: '/content_scripts/hide.js' });
+		browser.pageAction.setIcon({
+			tabId: tab.id,
+			path: '/icons/icon.svg',
+		});
 	} else {
 		console.log('show');
 		browser.tabs.executeScript(null, { file: '/content_scripts/show.js' });
+		browser.pageAction.setIcon({
+			tabId: tab.id,
+			path: '/icons/visibility.svg',
+		});
 	}
 	hide = !hide;
 });
