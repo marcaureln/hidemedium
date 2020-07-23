656a4ec1860abae2b91386585933e8d402121cf3
Alex N'Guessan <alexmarcaureln@gmail.com>
2020-07-23 01:56:10 +0000

    remove debugging console log

--- app.js
+++ app.js
@@ -3,9 +3,7 @@
 let hide = true;
 
 browser.pageAction.onClicked.addListener(function (tab) {
-	console.log('clicked');
 	if (hide) {
-		console.log('hide');
 		browser.tabs.executeScript(null, {
 			file: '/content_scripts/hide.js',
 			allFrames: true,
@@ -15,7 +13,6 @@ browser.pageAction.onClicked.addListener(function (tab) {
 			path: '/icons/icon.svg',
 		});
 	} else {
-		console.log('show');
 		browser.tabs.executeScript(null, {
 			file: '/content_scripts/show.js',
 			allFrames: true,
